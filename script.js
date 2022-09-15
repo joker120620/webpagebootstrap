537c497b6ca9c99ce4efc7054acccc6207c2278e
Juan Jose <juanjosemartinez1999@outlook.com>
2022-09-15 14:44:19 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function send(){
+    nombre = document.getElementById("name").value;
+    if(nombre!== ""){
+    alert("Mensaje enviado " + nombre +"." +"\nGracias por comunicarse con nosotros");
+}
+}
